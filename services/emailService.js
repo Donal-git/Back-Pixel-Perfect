5086621fdd53ef056f52fd0500970e38499b23d0
Donal-git <pagnagdoued@gmail.com>
2026-05-29 10:22:04 +0000

    Deuxième correction  d'envoie d'email automatique

--- services/emailService.js
+++ services/emailService.js
@@ -1,10 +1,15 @@
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
+import dns from 'dns';
+
+dns.setDefaultResultOrder('ipv4first');
+
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
+   family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
